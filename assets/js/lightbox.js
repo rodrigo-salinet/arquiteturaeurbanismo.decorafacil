3e54beb422a9b7ed17c132d65571ea0fb23b5ab5
rodrigo-salinet <rodrigo.salinet@gmail.com>
2023-04-03 18:03:31 -0300

    🐛 fix chg slides

--- assets/js/lightbox.js
+++ assets/js/lightbox.js
@@ -1,40 +1,78 @@
 // Open the Modal
-function openModal() {
-    document.getElementById("myModal").style.display = "block";
+const openModal = (modal_id) => {
+    document.getElementById(modal_id).style.display = "block";
 }
 
 // Close the Modal
-function closeModal() {
-    document.getElementById("myModal").style.display = "none";
-}
-
-var slideIndex = 1;
-    showSlides(slideIndex);
-
-// Next/previous controls
-function plusSlides(n) {
-    showSlides(slideIndex += n);
+const closeModal = (modal_obj) => {
+    modal_obj.style.display = "none";
 }
 
 // Thumbnail image controls
-function currentSlide(n) {
-    showSlides(slideIndex = n);
+const currentSlide = (n) => {
+    showSlide(slideIndex = n);
 }
 
-function showSlides(n) {
-    var i;
-    var slides = document.getElementsByClassName("mySlides");
-    var dots = document.getElementsByClassName("demo");
-    var captionText = document.getElementById("caption");
-    if (n > slides.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = slides.length}
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
+// Show / Change slides
+const showSlide = (n) => {
+    let slide1 = document.getElementById("slide1");
+    let slide2 = document.getElementById("slide2");
+    let slide3 = document.getElementById("slide3");
+    let slide4 = document.getElementById("slide4");
+    slide1.style.display = "none";
+    slide2.style.display = "none";
+    slide3.style.display = "none";
+    slide4.style.display = "none";
+
+    let img_slide1 = document.getElementById("img_slide1");
+    let img_slide2 = document.getElementById("img_slide2");
+    let img_slide3 = document.getElementById("img_slide3");
+    let img_slide4 = document.getElementById("img_slide4");
+    img_slide1.classList.remove("ativo");
+    img_slide2.classList.remove("ativo");
+    img_slide3.classList.remove("ativo");
+    img_slide4.classList.remove("ativo");
+
+    let img_slide_atual;
+    let slide_atual;
+
+    let captionText = document.getElementById("caption");
+
+    if (n < 1) {
+        slideIndex = 4;
+    } else if (n > 4) {
+        slideIndex = 1;
+    } else {
+        slideIndex = n;
     }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex-1].style.display = "block";
-    dots[slideIndex-1].className += " active";
-    captionText.innerHTML = dots[slideIndex-1].alt;
+
+    switch (slideIndex) {
+        case 1:
+            img_slide_atual = img_slide1;
+            slide_atual = slide1;
+            break;
+        case 2:
+            img_slide_atual = img_slide2;
+            slide_atual = slide2;
+            break;
+        case 3:
+            img_slide_atual = img_slide3;
+            slide_atual = slide3;
+            break;
+        case 4:
+            img_slide_atual = img_slide4;
+            slide_atual = slide4;
+            break;
+        default:
+            img_slide_atual = img_slide1;
+            slide_atual = slide1;
+    };
+
+    img_slide_atual.classList.add("ativo");
+    slide_atual.style.display = "block";
+    captionText.innerHTML = img_slide_atual.alt;
 }
+
+// Start slideIndex
+var slideIndex = 1;
+showSlide(slideIndex);
